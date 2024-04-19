9bedd2f168e11628c445c2fd24cb2bf366075b0e
BrendaWanjiru <brendawanjiru5@gmail.com>
2024-04-19 05:23:53 -0700

    starwars api

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,20 +1,17 @@
 #!/usr/bin/node
 
 const request = require('request');
-const movieId = parseInt(process.argv[2], 10);
 
-request.get(`https://swapi-api.alx-tools.com/api/films/${movieId}/`, async (error, response, body) => {
-  if (error) return;
-  const movie = JSON.parse(body);
-
-  for (const character of movie.characters) {
-    const characterName = await new Promise((resolve, reject) => {
-      request.get(character, (error, response, body) => {
-        if (error) return;
-        const characterInfo = JSON.parse(body);
-        resolve(characterInfo.name);
-      });
-    });
-    console.log(characterName);
-  }
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
+  if (err) throw err;
+  const actors = JSON.parse(body).characters;
+  exactOrder(actors, 0);
 });
+const exactOrder = (actors, x) => {
+  if (x === actors.length) return;
+  request(actors[x], function (err, res, body) {
+    if (err) throw err;
+    console.log(JSON.parse(body).name);
+    exactOrder(actors, x + 1);
+  });
+};
